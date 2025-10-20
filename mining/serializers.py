7fa686b6c039b641a61ad5a1b2db33b15f165946
Michael <awaemekamichael@gmail.com>
2025-10-20 14:18:14 +0100

    added postgres db

---
+++ mining/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+from .models import MiningSession
+
+
+
+
+class MiningSerializer(ModelSerializer):
+    
+    class Meta:
+        model = MiningSession
+        fields = '__all__'
--- mining/views.py
+++ mining/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+
+
+class UpdateMiningActivity(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()

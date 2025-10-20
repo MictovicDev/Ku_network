beb36544e90f7399bc06966f226cde32865b6c4a
Michael <awaemekamichael@gmail.com>
2025-10-20 14:37:03 +0100

    changes to image

--- news/models.py
+++ news/models.py
@@ -11,12 +11,13 @@ class News(models.Model):
     """News/Blog posts"""
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image1 = models.ImageField(upload_to='news/', blank=True, null=True)
-    image2 = models.ImageField(upload_to='news/', blank=True, null=True)
+    image = models.ImageField(upload_to='news/', blank=True, null=True)
+    # image2 = models.ImageField(upload_to='news/', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='news_posts')
     likes_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    note = models.TextField(blank=True, null=True)
 
     class Meta:
         ordering = ['-created_at']
--- news/serializers.py
+++ news/serializers.py
@@ -1,33 +1,27 @@
 from rest_framework import serializers
 from .models import News
 
+
 class NewsSerializer(serializers.ModelSerializer):
-    author_name = serializers.CharField(source='author.username', read_only=True)
-    image1_url = serializers.SerializerMethodField(read_only=True)
-    image2_url = serializers.SerializerMethodField(read_only=True)
+    author_name = serializers.CharField(
+        source='author.username', read_only=True)
+    image_url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = News
         fields = [
-            'id', 'title', 'content', 'image1', 'image2',
-            'image1_url', 'image2_url',
-            'author', 'author_name', 'likes_count',
-            'created_at', 'updated_at'
+            'id', 'title', 'content', 'image',
+            'image_url','author', 'likes_count',
+            'created_at', 'updated_at', 'note'
         ]
-        read_only_fields = ['likes_count', 'created_at', 'updated_at', 'author', 'image1_url', 'image2_url']
+        read_only_fields = ['likes_count', 'created_at',
+                            'updated_at', 'author', 'image1_url', 'image2_url']
 
-    def get_image1_url(self, obj):
-        if obj.image1:
+    def get_image_url(self, obj):
+        if obj.image:
             request = self.context.get('request')
             if request is not None:
-                return request.build_absolute_uri(obj.image1.url)
-            return obj.image1.url
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
         return None
 
-    def get_image2_url(self, obj):
-        if obj.image2:
-            request = self.context.get('request')
-            if request is not None:
-                return request.build_absolute_uri(obj.image2.url)
-            return obj.image2.url
-        return None
